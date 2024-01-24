2f1c34ae3ea66db1a25129ee819840d099c2d861
Vanessa-Salgado <vsalgadozavaleta@umail.ucsb.edu>
2024-01-24 17:16:49 +0000

    added lab02.ipynb and data

---
+++ labs/lab02/tests/q3_12.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3_12',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> print(empirical_P > 0.05)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}

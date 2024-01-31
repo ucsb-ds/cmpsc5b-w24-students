c87dd11f943e1582c608fadb6aa1fb32fabf59ae
annieradams <adams.anie11@gmail.com>
2024-01-31 06:10:26 +0000

    added hw02 to student server

---
+++ hw/hw02/tests/q1_2.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_2',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> len(percentages_in_resamples()) == 2500\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}

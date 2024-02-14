2ef705e76f7a332e739304672d140c44d7f18080
annieradams <adams.anie11@gmail.com>
2024-02-14 15:51:46 +0000

    updated to include test files

---
+++ projects/midterm/tests/q2_3.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2_3',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> type(comp_diff) == float\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> comp_diff > 40000\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}

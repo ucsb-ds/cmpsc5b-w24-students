060863c4b575df8c9fc5f32991b5074826ed8ea9
annieradams <adams.anie11@gmail.com>
2024-02-14 06:23:13 +0000

    added homework 3 to student repo

---
+++ hw/hw03(extracredit)/tests/q3_1.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3_1',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> len(resample_yes_proportions) == 10000\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> abs(np.mean(resample_yes_proportions) - 0.525) < 0.025 and np.std(resample_yes_proportions) < 0.08\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw03(extracredit)/tests/q3_3.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3_3',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> approximate_sd < 0.025\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}

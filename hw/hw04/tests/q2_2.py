fa51a3233a1ba33773caa80cd4afd9858901cd74
Sharon Solis <swsolis@ucsb.edu>
2024-02-21 18:32:40 +0000

    added hw04

---
+++ hw/hw04/tests/q2_2.py
@@ -0,0 +1,12 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2_2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> -1 <= r <= 1\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> standard_units(np.arange(5)) is None or np.allclose(standard_units(np.arange(5)), [-1.41421356, -0.70710678, 0, 0.70710678, 1.41421356])\nTrue',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw04/tests/q3_6.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3_6',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> np.round(outcome_errors.item(0), 2) == -25.25\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
